e83828fc8355b781c0a33fd416d8219949cff9b7
Tayyaba Shaikh <tayyaba.shaikh@websym.com>
2020-06-12 19:06:13 +0530

    Swagger added in api project

---
+++ API/Container/UnityResolverService.cs
@@ -0,0 +1,87 @@
+﻿// <copyright file = "UnityResolverService.cs" company="Websym Solutions Pvt Ltd">
+// Copyright (c) 2018 Websym Solutions Pvt Ltd.
+// </copyright>
+//-----------------------------------------------------------------------  
+
+namespace nIS
+{
+    #region References
+
+    using System;
+    using System.Collections.Generic;
+    using System.Web.Http.Dependencies;
+    using Unity;
+
+    #endregion
+
+    /// <summary>
+    /// Represents class for unity container.
+    /// </summary>
+
+    public class UnityResolverService : IDependencyScope
+    {
+        /// <summary>
+        /// IUnity container.
+        /// </summary>
+        protected IUnityContainer container;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Container"/> class.
+        /// </summary>
+        /// <param name="container">The container.</param>        
+        public UnityResolverService(IUnityContainer container)
+        {
+            if (container == null)
+            {
+                throw new ArgumentNullException("container");
+            }
+            this.container = container;
+        }
+
+        /// <summary>
+        /// Retrieves a service from the scope.
+        /// </summary>
+        /// <param name="serviceType">The service to be retrieved.</param>
+        /// <returns>
+        /// The retrieved service.
+        /// </returns>
+        public object GetService(Type serviceType)
+        {
+            try
+            {
+                return container.Resolve(serviceType);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Retrieves a collection of services from the scope.
+        /// </summary>
+        /// <param name="serviceType">The collection of services to be retrieved.</param>
+        /// <returns>
+        /// The retrieved collection of services.
+        /// </returns>
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            if (this.container.IsRegistered(serviceType))
+            {
+                return this.container.ResolveAll(serviceType);
+            }
+            else
+            {
+                return new List<object>();
+            }
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            this.container.Dispose();
+        }
+    }
+}
