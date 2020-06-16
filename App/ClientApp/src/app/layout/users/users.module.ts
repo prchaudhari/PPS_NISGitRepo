e3ec0a2bb64ef544e5ff936f828e99431195de5d
Tayyaba Shaikh <tayyaba.shaikh@websym.com>
2020-06-16 18:54:44 +0530

    User UI Binding

--- App/ClientApp/src/app/layout/users/users.module.ts
+++ App/ClientApp/src/app/layout/users/users.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { UsersRoutingModule } from './users-routing.module';
 import { ListComponent } from './list/list.component';
 import { ViewComponent } from './view/view.component';
-import { AddComponent } from './add/add.component';
+import { UserAddEditComponent } from './add/add-edit.component';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
+import { SharedModule } from '../../shared/modules/shared.module';
 
 @NgModule({
-  declarations: [ListComponent, ViewComponent, AddComponent],
+  declarations: [ListComponent, ViewComponent, UserAddEditComponent],
   imports: [
     CommonModule,
-      UsersRoutingModule, MatPaginatorModule, MatSortModule, MatTableModule
+    UsersRoutingModule, MatPaginatorModule, MatSortModule, MatTableModule, FormsModule,
+    ReactiveFormsModule, SharedModule
   ]
 })
 export class UsersModule { }
