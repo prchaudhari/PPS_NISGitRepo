ff9c33d043dbc4ee4e233302fc08870bea412fa0
Nishant Madaan <nishant.madaan@websym.com>
2020-06-18 14:50:03 +0530

    added base url in prod

---
+++ App/ClientApp/src/environments/environment.prod.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.environment = void 0;
+exports.environment = {
+    production: true,
+    baseURL: 'https://nis-api.azurewebsites.net/',
+};
+//# sourceMappingURL=environment.prod.js.map
