b7af94cd483b747caa749f9274a45981a9bbab97
Tayyaba Shaikh <tayyaba.shaikh@websym.com>
2020-07-28 23:56:54 +0530

    Analytics widget HMTL

--- Model/Common/HtmlConstants.cs
+++ Model/Common/HtmlConstants.cs
@@ -92,11 +92,14 @@ namespace nIS
             "<tbody><tr* ngFor = 'let list of actionList' >< td style='width:80%'><label style = 'background-color: #dce3dc;' >{{list.title}} </label></td>" +
             "<td style = 'float: left;' >< a >< i class='fa fa-caret-left fa-3x' style='color:red'aria-hidden='true'></i>{{list.action}}</a></td></tr></tbody></table> </div>  </div></div>";
 
-        public const string TOPINCOME_SOURCE_WIDGET_HTML = "<div class='card border-0'><div class='card-header bg-light border-0 text-left'><h5 class='m-0'>Top 4 Income Sources</h5>"+
-            "</div><div class='card-body'>div class='table-responsive'><table><thead><tr>"+"<td style='width:50%'></td><td style='width:15%'>This Month</td><td style='width:35%;float:center;'>Usually you spend</td></tr>"+
-            "</thead><tbody><tr *ngFor='let list of actionList'><td style='width:50%'><label>{{list.name}} </label></td>"+
-            "<td style='width:15%'><label>{{list.thisMonth}} </label> </td><td style='width:35%;float:center'><label> <span style='color: red' class='{{list.icon}}' aria-hidden='true'></span>{{list.usuallySpend}} </label>"+
+        public const string TOPINCOME_SOURCE_WIDGET_HTML = "<div class='card border-0'><div class='card-header bg-light border-0 text-left'><h5 class='m-0'>Top 4 Income Sources</h5>" +
+            "</div><div class='card-body'>div class='table-responsive'><table><thead><tr>" + "<td style='width:50%'></td><td style='width:15%'>This Month</td><td style='width:35%;float:center;'>Usually you spend</td></tr>" +
+            "</thead><tbody><tr *ngFor='let list of actionList'><td style='width:50%'><label>{{list.name}} </label></td>" +
+            "<td style='width:15%'><label>{{list.thisMonth}} </label> </td><td style='width:35%;float:center'><label> <span style='color: red' class='{{list.icon}}' aria-hidden='true'></span>{{list.usuallySpend}} </label>" +
             "</td></tr></tbody></table></div></div></div>";
+
+        public const string ANALYTIC_WIDGET_HTML = "<div class='card border-0'>< div class='card-header bg-light border-0 text-left'> <h5 class='m-0'>Analytics</h5></div>" +
+        "<div class='card-body'> <div id = 'chartWidgetPiecontainer' ></ div ></ div ></ div > ";
 
         public const string CONTAINER_DIV_HTML_HEADER = "<div class='container-fluid mt-3 mb-3 bdy-scroll stylescrollbar'>";
 
