c2072bc5afeb3b74a398b19cb4d80f3adc1a3511
pramod.shinde <pramod.shinde@websym.com>
2020-08-28 10:57:31 +0000

    Updated AnalyticsData.sql

--- Store/AnalyticsData.sql
+++ Store/AnalyticsData.sql
@@ -3,9 +3,11 @@
 	[Id] BIGINT IDENTITY(1,1) NOT NULL PRIMARY KEY, 
     [StatementId] BIGINT NOT NULL, 
     [CustomerId] BIGINT NOT NULL, 
+    [AccountId] BIGINT NULL, 
+    [PageWidgetId] BIGINT NULL, 
     [PageId] BIGINT NULL, 
     [WidgetId] BIGINT NULL, 
     [EventDate] DATETIME NOT NULL, 
     [EventType] NVARCHAR(50) NOT NULL, 
-
+    [TenantCode] NVARCHAR(50) NOT NULL, 
 )
