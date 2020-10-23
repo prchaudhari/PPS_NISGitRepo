ae02f4a29e1fb80166c5aebf5f2f227622c114fa
Pramod <pramod.shinde@websym.com>
2020-10-23 09:43:10 +0530

    deployment error changes.

--- App/ClientApp/src/environments/environment.prod.ts
+++ App/ClientApp/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   //baseURL: 'https://nis-api.azurewebsites.net/',
   baseURL: 'http://nisqa-api.azurewebsites.net/',
-  appVersion: '2.1.5'
+  appVersion: '2.1.7'
 };
