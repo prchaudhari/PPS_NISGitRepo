d8dd105cbec474f8e5097d9385eee928b288b610
Pramod <pramod.shinde@websym.com>
2020-11-20 12:46:06 +0530

    archival process web job added in webjobs-list.json, so it can get deployed with api service

--- App/ClientApp/src/environments/environment.prod.ts
+++ App/ClientApp/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   //baseURL: 'https://nis-api.azurewebsites.net/',
   baseURL: 'http://nisqa-api.azurewebsites.net/',
-  appVersion: '2.2.3'
+  appVersion: '2.2.5'
 };
