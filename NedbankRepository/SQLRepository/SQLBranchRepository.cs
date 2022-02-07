09d8d7739c6b85a152b1a139db3e0981077fdead
Deepak Rai <deepak.rai@websym.com>
2022-02-07 16:25:13 +0530

    Simplified SQLBranchRepository.cs

--- NedbankRepository/SQLRepository/SQLBranchRepository.cs
+++ NedbankRepository/SQLRepository/SQLBranchRepository.cs
@@ -31,26 +31,6 @@ namespace NedbankRepository
 
         NedbankEntities db;
 
-        /// <summary>
-        /// The validation engine object
-        /// </summary>
-        INedBankValidationEngine validationEngine = null;
-
-        /// <summary>
-        /// The connection string
-        /// </summary>
-        private string connectionString = string.Empty;
-
-        /// <summary>
-        /// The unity container
-        /// </summary>
-        private IUnityContainer unityContainer = null;
-
-        /// <summary>
-        /// The configurationutility
-        /// </summary>
-        private IConfigurationUtility configurationutility = null;
-
         #endregion
 
         #region Constructor
@@ -61,10 +41,6 @@ namespace NedbankRepository
         /// <param name="unityContainer">The unity container.</param>
         public SQLBranchRepository(IUnityContainer unityContainer)
         {
-            this.unityContainer = unityContainer;
-            this.validationEngine = new NedBankValidationEngine();
-            this.configurationutility = new ConfigurationUtility(this.unityContainer);
-
             db = new NedbankEntities();
         }
 
@@ -99,51 +75,6 @@ namespace NedbankRepository
             };
         }
 
-
-        #endregion
-
-        #region Private Method
-
-        /// <summary>
-        /// This method help to set and validate connection string
-        /// </summary>
-        /// <param name="tenantCode">The tenant code</param>
-        /// <exception cref="NedBankException.ConnectionStringNotFoundException"></exception>
-        private void SetAndValidateConnectionString(string tenantCode)
-        {
-            try
-            {
-                this.connectionString = this.configurationutility.GetConnectionString(ModelConstant.COMMON_SECTION, ModelConstant.NIS_CONNECTION_STRING, ModelConstant.CONFIGURATON_BASE_URL, ModelConstant.TENANT_CODE_KEY, tenantCode);
-                if (!this.validationEngine.IsValidText(this.connectionString))
-                {
-                    throw new ConnectionStringNotFoundException(tenantCode);
-                }
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-
-        /// <summary>
-        /// Generate string for dynamic linq.
-        /// </summary>
-        /// <param name="branchId">The investor identifier.</param>
-        /// <param name="tenantCode">The tenant code.</param>
-        /// <returns>
-        /// Returns a string.
-        /// </returns>
-        private string WhereClauseGenerator(long branchId, string tenantCode)
-        {
-            StringBuilder queryString = new StringBuilder();
-            if (tenantCode != ModelConstant.DEFAULT_TENANT_CODE)
-            {
-                queryString.Append(string.Format(" TenantCode.Equals(\"{0}\") and", tenantCode));
-            }
-            queryString.Append(string.Format(" BranchId.Equals({0})", branchId));
-
-            return queryString.ToString();
-        }
 
         #endregion
     }
